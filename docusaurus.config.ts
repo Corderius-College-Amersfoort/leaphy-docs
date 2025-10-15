3b426c7fe37746cc1e0a5642d2b4599df62702b7
Marten Postma <postmam@corderius.nl>
2025-10-15 13:58:44 +0200

    link met overzicht

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,6 +86,11 @@ const config: Config = {
           docsPluginId: 'lego_auto',
         },
         {
+          to: '/docs/Competities/RoboCup-Junior/Redden-Basis/start',
+          label: 'Redden basis overzicht',
+          position: 'left'
+        },
+        {
           href: 'https://github.com/Corderius-College-Amersfoort/leaphy-docs',
           label: 'GitHub',
           position: 'right',
